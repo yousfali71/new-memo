8ad3cc7718dbfbcb6be8f78e4dcded5ce7fdb910
yousfali71 <164696566+yousfali71@users.noreply.github.com>
2024-09-05 20:21:02 +0300

    Initial commit

---
+++ js/memo.js
@@ -0,0 +1,32 @@
+const notesContainer = document.querySelector(".notes-container");
+const creatBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+function updatestorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+creatBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updatestorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updatestorage();
+      };
+    });
+  }
+});
